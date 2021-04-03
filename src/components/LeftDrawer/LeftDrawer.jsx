502066593621a7a33f33973634d78366ed0b46ea
William Krug <william.p.krug@gmail.com>
2021-04-03 12:58:39 -0500

    GitHub and LinkedIn icons at bottom of drawer and open new tabs when clicked

--- src/components/LeftDrawer/LeftDrawer.jsx
+++ src/components/LeftDrawer/LeftDrawer.jsx
@@ -13,6 +13,7 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
@@ -66,12 +67,14 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       color: '#ED2939',
     },
+    margin: 15,
   },
 }));
 
 function LeftDrawer() {
   const classes = useStyles();
   const history = useHistory();
+  const preventDefault = (event) => event.preventDefault();
 
   return (
     <div className={classes.root}>
@@ -156,53 +159,32 @@ function LeftDrawer() {
           </ListItem>
         </List>
         <Box>
-          <GitHubIcon
-            onClick={(event) =>
-              (window.location.href = 'https://github.com/William-Krug')
-            }
-            className={classes.clickableIcon}
-          />
-          <LinkedInIcon
-            onClick={(event) =>
-              (window.location.href =
-                'https://www.linkedin.com/in/william-krug-67a86ab6/')
-            }
-            className={classes.clickableIcon}
-          />
+          <Grid container justify="center" alignItems="flex-end">
+            <Grid item>
+              <Box mt={45}>
+                <Link
+                  href="https://github.com/William-Krug"
+                  target="_blank"
+                  rel="noreferrer"
+                  color="inherit"
+                  onClick={preventDefault}
+                >
+                  <GitHubIcon className={classes.clickableIcon} />
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/william-krug-67a86ab6/"
+                  target="_blank"
+                  rel="noreferrer"
+                  color="inherit"
+                  onClick={preventDefault}
+                >
+                  <LinkedInIcon className={classes.clickableIcon} />
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Drawer>
-      {/* <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </main> */}
     </div>
   );
 }
